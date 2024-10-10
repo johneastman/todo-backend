fc676ef22090631d81d24ba7f09624aae7cb5ce5
John Eastman <jp.robotics@gmail.com>
2024-10-09 19:59:22 -0600

    restructure tests

--- test_user_data.py
+++ test_user_data.py
@@ -27,49 +27,6 @@ def runner(application):
     return app.test_cli_runner()
 
 
-def test_get_all_users(client):
-    response = client.get("/users")
-    assert response.status_code == 200
-
-    body = json.loads(response.data)
-    assert body == ["test"]
-
-
-def test_check_if_user_exists_when_user_exists(client):
-    response = client.get("/users/test", query_string={"checkUserExists": "true"})
-    assert response.status_code == 200
-
-    body = json.loads(response.data)
-    assert body["username"] == "test"
-    assert body["exists"] == True
-
-
-def test_check_if_user_exists_when_user_does_not_exists(client):
-    response = client.get("/users/invalidUser", query_string={"checkUserExists": "true"})
-    assert response.status_code == 200
-
-    body = json.loads(response.data)
-    assert body["username"] == "invalidUser"
-    assert body["exists"] == False
-
-
-def test_get_user_data(client):
-    response = client.get("/users/test")
-    assert response.status_code == 200
-
-    body = json.loads(response.data)
-    assert body["listsJSON"] is not None
-    assert body["settingsJSON"] is not None
-
-
-def test_get_user_data_does_not_exist(client):
-    response = client.get("/users/invalidUser")
-    assert response.status_code == 404
-
-    body = json.loads(response.data)
-    assert body["message"] == "User does not exist"
-
-
 def test_save_user_data(client):
     response = client.post("/users/test2", json={
         "listsJSON": [
@@ -102,6 +59,49 @@ def test_save_user_data(client):
 
     body = json.loads(response.data)
     assert body["message"] == "Data saved successfully"
+
+
+def test_check_if_user_exists(client):
+    response = client.get("/users/test", query_string={"checkUserExists": "true"})
+    assert response.status_code == 200
+
+    body = json.loads(response.data)
+    assert body["username"] == "test"
+    assert body["exists"] == True
+
+
+def test_get_all_users(client):
+    response = client.get("/users")
+    assert response.status_code == 200
+
+    body = json.loads(response.data)
+    assert body == ["test"]
+
+
+def test_get_user_data(client):
+    response = client.get("/users/test")
+    assert response.status_code == 200
+
+    body = json.loads(response.data)
+    assert body["listsJSON"] is not None
+    assert body["settingsJSON"] is not None
+
+
+def test_check_if_user_does_not_exists(client):
+    response = client.get("/users/invalidUser", query_string={"checkUserExists": "true"})
+    assert response.status_code == 200
+
+    body = json.loads(response.data)
+    assert body["username"] == "invalidUser"
+    assert body["exists"] == False
+
+
+def test_get_user_data_that_does_not_exist(client):
+    response = client.get("/users/invalidUser")
+    assert response.status_code == 404
+
+    body = json.loads(response.data)
+    assert body["message"] == "User does not exist"
 
 
 def test_save_user_data_invalid_json(client):
