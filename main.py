7e7131d18eccc61feb7c1b74ab0d9befd66de102
John Eastman <jp.robotics@gmail.com>
2024-10-09 21:19:12 -0600

    refactor api

--- main.py
+++ main.py
@@ -1,73 +1,79 @@
 from flask import Flask, Response, request, make_response
 from dotenv import dotenv_values
 from mysql import connector
+from mysql.connector.abstracts import MySQLCursorAbstract, MySQLConnectionAbstract
 
 import os
 import json
+from functools import wraps
+
+from mysql.connector.pooling import PooledMySQLConnection
 
 app = Flask(__name__)
 
 ROOT_DIR = os.path.expanduser(app.root_path)
 
 config = dotenv_values(os.path.join(ROOT_DIR, ".env"))
-print("Environment data", config)
 
 
-# Database Setup
-def connect_to_db(name):
-    """Initiates a connection to the database
+def db_conn(method):
 
-    :param name: name of the database
-    :type name: str
-    :return: database object and cursor
-    :rtype: (PooledMySQLConnection | MySQLConnectionAbstract,  MySQLCursorAbstract)
-    """
-    db = connector.connect(
-        host=config.get("DB_HOST"),
-        user=config.get("DB_USER"),
-        password=config.get("DB_PASSWORD"),
-        database=name
-    )
-    return db, db.cursor(buffered=True)
+    @wraps(method)
+    def inner(*args, **kwargs):
+        with connector.connect(
+            host=config.get("DB_HOST"),
+            user=config.get("DB_USER"),
+            password=config.get("DB_PASSWORD"),
+            database=config.get("DB_NAME")
+        ) as connection:
+            with connection.cursor(buffered=True) as cursor:
+                return method(connection, cursor, *args, **kwargs)
+
+    return inner
 
 
-db_name = config.get("DB_NAME")
-setup_db, setup_cursor = connect_to_db(db_name)
+@db_conn
+def setup(
+        connection: PooledMySQLConnection | MySQLConnectionAbstract,
+        cursor: MySQLCursorAbstract):
 
-setup_cursor.execute(f"create database if not exists {db_name}")
-setup_cursor.execute(f"use {db_name}")
-setup_cursor.execute(
-    "create table if not exists users(id int primary key auto_increment, name text not null, data text not null)")
+    db_name = config.get("DB_NAME")
 
-setup_cursor.close()
-setup_db.disconnect()
+    cursor.execute(f"create database if not exists {db_name}")
+    cursor.execute(f"use {db_name}")
+    cursor.execute(
+        "create table if not exists users(id int primary key auto_increment, name text not null, data text not null)")
+
+
+# Initialize the database
+setup()
 
 
 @app.route("/users")
-def get_users():
-    get_users_db, get_users_cursor = connect_to_db(db_name)
-    get_users_cursor.execute("select name from users")
-    results = get_users_cursor.fetchall()
+@db_conn
+def get_users(
+        connection: PooledMySQLConnection | MySQLConnectionAbstract,
+        cursor: MySQLCursorAbstract):
 
-    get_users_cursor.close()
-    get_users_db.disconnect()
-
+    cursor.execute("select name from users")
+    results = cursor.fetchall()
     return make_response(list(map(lambda row: row[0], results)), 200)
 
 
 @app.route("/users/<username>", methods=["GET", "POST", "DELETE"])
-def get_user(username):
+@db_conn
+def get_user(
+        connection: PooledMySQLConnection | MySQLConnectionAbstract,
+        cursor: MySQLCursorAbstract,
+        username: str):
+
     if request.args.get("checkUserExists") == "true":
 
-        get_user_db, get_user_cursor = connect_to_db(db_name)
-        get_user_cursor.execute(
+        cursor.execute(
             "select * from users where name = %s",
             (username,)
         )
-        result = get_user_cursor.fetchone()
-
-        get_user_cursor.close()
-        get_user_db.disconnect()
+        result = cursor.fetchone()
 
         body = {"username": username, "exists": result is not None}
         return make_response(body, 200)
@@ -78,7 +84,7 @@ def get_user(username):
         "DELETE": delete
     }
 
-    return methods[request.method](username)
+    return methods[request.method](connection, cursor, username)
 
 
 @app.route("/config", methods=["GET"])
@@ -86,17 +92,17 @@ def get_config():
     return make_response(config, 200)
 
 
-def get(username: str) -> Response:
+def get(
+        connection: PooledMySQLConnection | MySQLConnectionAbstract,
+        cursor: MySQLCursorAbstract,
+        username: str) -> Response:
+
     try:
-        get_db, get_cursor = connect_to_db(db_name)
-        get_cursor.execute(
+        cursor.execute(
             "select data from users where name = %s",
             (username,)
         )
-        result = get_cursor.fetchone()
-
-        get_cursor.close()
-        get_db.disconnect()
+        result = cursor.fetchone()
 
         if result is None:
             return make_response({"message": "User does not exist"}, 404)
@@ -105,30 +111,25 @@ def get(username: str) -> Response:
         return make_response(body, 200)
     except Exception as e:
         print(e)
-        return make_response({"message": "Internal Server Error"}, 500)
+        return make_response({"message": "Unable to retrieve user data"}, 500)
 
 
-def save(username: str) -> Response:
+def save(connection: PooledMySQLConnection | MySQLConnectionAbstract, cursor: MySQLCursorAbstract, username: str) -> Response:
     try:
         user_data = json.dumps(request.json)
 
-        save_db, save_cursor = connect_to_db(db_name)
-
-        save_cursor.execute("select * from users where name = %s", (username,))
-        result = save_cursor.fetchone()
+        cursor.execute("select * from users where name = %s", (username,))
+        result = cursor.fetchone()
         if result is not None:
             # Update data if a row already exists
             print(f"Updating data for {username}")
-            save_cursor.execute("update users set data = %s where name = %s", (user_data, username))
+            cursor.execute("update users set data = %s where name = %s", (user_data, username))
         else:
             # Insert new row if the row does not exist
             print(f"Creating data for {username}")
-            save_cursor.execute("insert into users (name, data) values (%s, %s)", (username, user_data))
+            cursor.execute("insert into users (name, data) values (%s, %s)", (username, user_data))
 
-        save_db.commit()
-
-        save_cursor.close()
-        save_db.disconnect()
+        connection.commit()
 
         return make_response({"message": "Data saved successfully"}, 200)
     except Exception as e:
@@ -136,23 +137,22 @@ def save(username: str) -> Response:
         return make_response({"message": "Failed to save user data"}, 500)
 
 
-def delete(username: str) -> Response:
-    try:
-        delete_db, delete_cursor = connect_to_db(db_name)
+def delete(
+        connection: PooledMySQLConnection | MySQLConnectionAbstract,
+        cursor: MySQLCursorAbstract,
+        username: str) -> Response:
 
+    try:
         # Check if the user exists.
-        delete_cursor.execute("select * from users where name = %s", (username,))
-        result = delete_cursor.fetchone()
+        cursor.execute("select * from users where name = %s", (username,))
+        result = cursor.fetchone()
         if result is None:
             return make_response({"message": "User not found"}, 404)
 
         # If the user does exist, delete their row from the database.
-        delete_cursor.execute("delete from users where name = %s", (username,))
+        cursor.execute("delete from users where name = %s", (username,))
 
-        delete_db.commit()
-
-        delete_cursor.close()
-        delete_db.disconnect()
+        connection.commit()
 
         return make_response({"message": "Data successfully deleted"}, 200)
 
